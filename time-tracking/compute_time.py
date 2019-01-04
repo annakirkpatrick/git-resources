a377637ea359baab5561c7159ca200b26ffb8f31
Anna Kirkpatrick <akirkpatrick3@gatech.edu>
2019-01-04 18:26:01 -0500

    finish time tracking summation and output 1h15m

--- time-tracking/compute_time.py
+++ time-tracking/compute_time.py
@@ -46,16 +46,50 @@ if __name__ == '__main__':
     hour_only = re.compile('\d+[hH]')
     both_hour_min = re.compile('\d+[hH]\d+[mM]')
 
+    #and set up a tally
+    total_mins = 0
+
     #read newly-written log
     #include delay because /projects is slow
     time.sleep(1)
     with open(file_name, 'r') as log_file:
         for line in log_file:
+            mins=0
             if line.startswith("#"): #header comment lines we wrote
+                continue
+            if line == "": #ignore any blank lines
                 continue
             words = line.split() #split on whitespace
             time_string = words[len(words) - 1] #grab last chunk of each log entry
             #if a time tracking entry exists, it should be here
+            try:
+                if not min_only.match(time_string) is None:
+                    mins_str = time_string[:-1]
+                    mins=int(mins_str)
+                elif not hour_only.match(time_string) is None:
+                    hours_str = time_string[:-1]
+                    mins=int(hours_str)*60
+                elif not both_hour_min.match(time_string) is None:
+                    h_pos = 0
+                    try:
+                        h_pos = time_string.index("h")
+                    except ValueError:
+                        h_pos = time_string.index("H")
+                    hours_str = time_string[:h_pos]
+                    mins_str = time_string[h_pos+1:-1]
+                    mins = 60*int(hours_str) + int(mins_str)
+                else: #no time record for this commit
+                    print("Info: No time record found for commit:\n" + line)
+            except ValueError:
+                print("Error: failed to parse commit, no time will be added")
+            if mins > 120: #shouldn't happen, so print a warning
+                print("Warning: {0} minutes logged on commit:\n{1}".format(mins, line))
+            total_mins+=mins
+
+    display_hours = total_mins // 60 #will work with python 2 or 3
+    display_mins = total_mins % 60
+    print("Total Time Recorded:  {0} hours and {1} minutes".format(display_hours, display_mins))
+
 
 
 
